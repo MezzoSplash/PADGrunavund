5139bad9390fcb8f69ab1156bb6ebdf4220e3c0c
AsianPizza <42935788+AsianPizza@users.noreply.github.com>
2019-03-31 11:33:45 +0200

    Add files via upload

--- Movement.py
+++ Movement.py
@@ -1,13 +1,13 @@
 import bge
+import bpy
 
 def main():
 
     cont = bge.logic.getCurrentController()
     player = cont.owner
-
+    
     scene = bge.logic.getCurrentScene()
     potion = scene.objectsInactive['Potion']
-    level = scene.objects['Level']
     keyboard = bge.logic.keyboard
     mouse = bge.logic.mouse
 
@@ -15,22 +15,19 @@ def main():
         player.localPosition.x += 0.1
 
     if bge.logic.KX_INPUT_ACTIVE == keyboard.events[bge.events.AKEY]:
-        player.localPosition.x += -0.1
+        player.localPosition.x -= 0.1
 
     if bge.logic.KX_INPUT_ACTIVE == keyboard.events[bge.events.WKEY]:
         player.localPosition.y += 0.1
         
     if bge.logic.KX_INPUT_ACTIVE == keyboard.events[bge.events.SKEY]:
         player.localPosition.y -= 0.1
-
-    if bge.logic.KX_INPUT_ACTIVE == keyboard.events[bge.events.SPACEKEY]:
-        new_object = scene.addObject(Potion, player)
-
+        
     mouseClick = bge.logic.KX_INPUT_JUST_RELEASED == mouse.events[bge.events.LEFTMOUSE]
 
-    life_time = 40
-
+    life_time = 120
+    
     if mouseClick:
-        scene.addObject(potion, player)
+        new_potion = scene.addObject(potion, player, life_time)
 
 main()
