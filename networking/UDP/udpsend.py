416a211cfd4d25beab5ecbeb893a066bd1e3e34e
MezzoSplash <43997926+MezzoSplash@users.noreply.github.com>
2019-05-20 15:43:27 +0200

    gun toekomst
    help

--- networking/UDP/udpsend.py
+++ networking/UDP/udpsend.py
@@ -1,7 +1,7 @@
 import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-MESSAGE = "hello world"
+MESSAGE = "2"
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
 print("message:", MESSAGE)
